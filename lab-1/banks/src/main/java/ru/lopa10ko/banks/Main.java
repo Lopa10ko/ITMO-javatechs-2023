cd740ea7883a00234f01a6e7beee4e25ce291019
lopa10ko <lopa10ko239@mail.ru>
2023-02-28 22:40:11 +0300

    feat: add gradle build init

--- lab-1/banks/src/main/java/ru/lopa10ko/banks/Main.java
+++ lab-1/banks/src/main/java/ru/lopa10ko/banks/Main.java
@@ -1,7 +1,5 @@
 package ru.lopa10ko.banks;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args) { }
 }
