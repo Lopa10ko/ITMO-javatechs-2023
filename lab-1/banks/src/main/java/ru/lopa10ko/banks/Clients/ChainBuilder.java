d73bbe7e5c19386e876c0ba11e311b5cdb5764ea
lopa10ko <lopa10ko239@mail.ru>
2023-03-03 11:32:27 +0300

    feat: add minimum javadoc comments

--- lab-1/banks/src/main/java/ru/lopa10ko/banks/Clients/ChainBuilder.java
+++ lab-1/banks/src/main/java/ru/lopa10ko/banks/Clients/ChainBuilder.java
@@ -1,7 +1,23 @@
 package ru.lopa10ko.banks.Clients;
 
 public interface ChainBuilder {
+    /**
+     * Builder method to access address to ClientBuilder
+     * @param address raw string value, representing address of a client
+     * @return ChainBuilder to continue building process
+     */
     ChainBuilder withAddress(String address);
+
+    /**
+     * Builder method to access passport to ClientBuilder
+     * @param passport raw string value, representing passport of a client
+     * @return ChainBuilder to continue building process
+     */
     ChainBuilder withPassport(String passport);
+
+    /**
+     *
+     * @return BasicClient built entity
+     */
     BasicClient build();
 }
