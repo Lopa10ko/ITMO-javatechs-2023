c196d0a707d698e07657bf83d6f7a20ca3ab07b5
Lopa10ko <justforprojects@mail.ru>
2023-04-11 21:30:25 +0300

    feat: add rest controllers

--- lab-2/presentation/src/main/java/ru/lopa10ko/cats/controllers/CatControllerImpl.java
+++ lab-2/presentation/src/main/java/ru/lopa10ko/cats/controllers/CatControllerImpl.java
@@ -1,53 +1,29 @@
 package ru.lopa10ko.cats.controllers;
 
 import lombok.AllArgsConstructor;
-import ru.lopa10ko.cats.commons.CatColor;
+import org.springframework.web.bind.annotation.*;
+import ru.lopa10ko.cats.controllers.requests.CreateCatRequest;
 import ru.lopa10ko.cats.dto.CatDto;
 import ru.lopa10ko.cats.services.CatFacade;
 
-import java.time.LocalDate;
 import java.util.UUID;
 
 @AllArgsConstructor
-public class CatControllerImpl implements CatController {
+@RequestMapping("/cats")
+@RestController
+public class CatControllerImpl {
     private final CatFacade catFacade;
-
-
-    /**
-     * Double Service creation method
-     *
-     * @param name     string
-     * @param birthDay LocalDate
-     * @param breed    string
-     * @param catColor CatColor
-     * @return CatDto
-     * @see CatDto
-     */
-    @Override
-    public CatDto createCat(String name, LocalDate birthDay, String breed, CatColor catColor) {
-        return catFacade.createCat(name, birthDay, breed, catColor);
+    @PostMapping
+    public CatDto createCat(@RequestBody CreateCatRequest createCatRequest) {
+        return catFacade.createCat(createCatRequest.getName(), createCatRequest.getBirthDay(), createCatRequest.getBreed(), createCatRequest.getCatColor());
     }
 
-    /**
-     * Double Service reading method
-     *
-     * @param catUuid id to read
-     * @return CatDto
-     * @see CatDto
-     */
-    @Override
-    public CatDto readCat(UUID catUuid) {
+    @GetMapping("/{id}")
+    public CatDto readCat(@PathVariable("id") UUID catUuid) {
         return catFacade.readCat(catUuid);
     }
-
-    /**
-     * Double Service deletion method
-     *
-     * @param catUuid id to delete
-     * @see CatDto
-     */
-    @Override
-    public void deleteCat(UUID catUuid) {
+    @DeleteMapping("/{id}")
+    public void deleteCat(@PathVariable("id") UUID catUuid) {
         catFacade.deleteCat(catUuid);
     }
 }
