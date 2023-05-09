4a504326fbef4127a92f5a315e9c5e45bfd8a1e8
Lopa10ko <justforprojects@mail.ru>
2023-05-09 19:32:14 +0300

    feat: add project specific authentication type

---
+++ lab-2/security/src/main/java/ru/lopa10ko/cats/security/model/CatsAuthentication.java
@@ -0,0 +1,51 @@
+package ru.lopa10ko.cats.security.model;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.UUID;
+
+@AllArgsConstructor
+public class CatsAuthentication implements Authentication {
+    private String userName;
+    private Set<Roles> roles;
+    private UUID uuid;
+    private boolean authenticated;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+        authenticated = isAuthenticated;
+    }
+
+    @Override
+    public String getName() {
+        return userName;
+    }
+}
