f9587eaed91eb5869f95455ce891431fcd69ef1d
Lopa10ko <justforprojects@mail.ru>
2023-05-09 19:22:55 +0300

    feat: implement JwtService interface providing token construction and decomposition

---
+++ lab-2/security/src/main/java/ru/lopa10ko/cats/security/services/JwtServiceImpl.java
@@ -0,0 +1,82 @@
+package ru.lopa10ko.cats.security.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import ru.lopa10ko.cats.entities.User;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+public class JwtServiceImpl implements JwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(User userDetails) {
+        return buildToken(userDetails, jwtExpiration);
+    }
+
+    private String buildToken(User userDetails, long expiration) {
+        return Jwts
+                .builder()
+                .setSubject(userDetails.getUsername())
+                .claim("roles", userDetails.getRoles())
+                .claim("ownerUuid", userDetails.getCatOwner().getUuid())
+                .claim("userUuid", userDetails.getUuid())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSignInKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception exception) {
+            System.out.println("Error");
+        }
+        return false;
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
