9d4d5f530f8db5a2a0c53d680e6d05bc6ddd0d03
Lopa10ko <justforprojects@mail.ru>
2023-05-09 19:18:20 +0300

    feat: initialize AuthController

---
+++ lab-2/presentation/src/main/java/ru/lopa10ko/cats/controllers/AuthControllerImpl.java
@@ -0,0 +1,9 @@
+package ru.lopa10ko.cats.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthControllerImpl {
+}
