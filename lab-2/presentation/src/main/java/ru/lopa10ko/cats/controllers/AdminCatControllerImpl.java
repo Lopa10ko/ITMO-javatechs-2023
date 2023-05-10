8c0a2df31e6cb16c7aaa4c4e146958ede3d32aea
Lopa10ko <justforprojects@mail.ru>
2023-05-10 22:26:48 +0300

    feat: add ADMIN specific controllers

---
+++ lab-2/presentation/src/main/java/ru/lopa10ko/cats/controllers/AdminCatControllerImpl.java
@@ -0,0 +1,55 @@
+package ru.lopa10ko.cats.controllers;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.lopa10ko.cats.commons.CatColor;
+import ru.lopa10ko.cats.commons.exceptions.CreationException;
+import ru.lopa10ko.cats.controllers.requests.CreateCatRequest;
+import ru.lopa10ko.cats.dto.CatDto;
+import ru.lopa10ko.cats.services.CatFacade;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@RequestMapping("/api/admin/cats")
+@RestController
+@PreAuthorize("hasAuthority('ADMIN')")
+@SecurityRequirement(name = "Bearer Authentication")
+public class AdminCatControllerImpl {
+    private final CatFacade catFacade;
+    @PostMapping
+    public CatDto createCat(@Valid @RequestBody CreateCatRequest createCatRequest, BindingResult result) {
+        if (result.hasErrors()) {
+            throw CreationException.throwException();
+        }
+        return catFacade.createCat(createCatRequest.getName(), createCatRequest.getCatOwnerUuid(), createCatRequest.getBirthDay(), createCatRequest.getBreed(), CatColor.valueOf(createCatRequest.getCatColor()));
+    }
+
+    @GetMapping("/{id}")
+    public CatDto readCat(@PathVariable("id") UUID catUuid) {
+        return catFacade.readCat(catUuid);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteCat(@PathVariable("id") UUID catUuid) {
+        catFacade.deleteCat(catUuid);
+    }
+
+    @GetMapping
+    public List<CatDto> getFilteredCats(@RequestParam(defaultValue = "") List<String> name,
+                                        @RequestParam(defaultValue = "") List<UUID> uuid,
+                                        @RequestParam(defaultValue = "") List<LocalDate> birthDay,
+                                        @RequestParam(defaultValue = "") List<CatColor> color,
+                                        @RequestParam(defaultValue = "") List<String> breed) {
+        return catFacade.getByParams(name, uuid, birthDay, color, breed);
+    }
+    @PatchMapping("/{id}/{friendId}")
+    public void addCatFriend(@PathVariable("id") UUID catUuid, @PathVariable("friendId") UUID catFriendUuid) {
+        catFacade.addFriend(catUuid, catFriendUuid);
+    }
+}
