7ccd7b7deb58d5da17cde6e4d134318a783626d3
Lopa10ko <justforprojects@mail.ru>
2023-05-10 22:27:08 +0300

    feat: add authentication specific controllers

--- lab-2/presentation/src/main/java/ru/lopa10ko/cats/controllers/AuthControllerImpl.java
+++ lab-2/presentation/src/main/java/ru/lopa10ko/cats/controllers/AuthControllerImpl.java
@@ -1,9 +1,24 @@
 package ru.lopa10ko.cats.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.lopa10ko.cats.controllers.requests.JwtRequest;
+import ru.lopa10ko.cats.controllers.response.JwtResponse;
+import ru.lopa10ko.cats.services.JwtAuthService;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthControllerImpl {
+    private final JwtAuthService jwtAuthService;
+    @PostMapping("/register")
+    public ResponseEntity<JwtResponse> register(@RequestBody JwtRequest request) {
+        return ResponseEntity.ok(jwtAuthService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<JwtResponse> authenticate(@RequestBody JwtRequest request) {
+        return ResponseEntity.ok(jwtAuthService.authenticate(request));
+    }
 }
